167dc30e71fec82b78f89bd11eb88f6b7e3d4b4c
Collin Erickson <collinberickson@gmail.com>
2017-01-14 18:36:56 -0600

    Added name options
    Didn't want to use tolower since e.g. I want GPfit to stay as GPfit not gpfit.

--- R/UGP2_selector.R
+++ R/UGP2_selector.R
@@ -15,21 +15,21 @@
 UGP2 <- function(X=NULL, Z=NULL, package=NULL, ...) {
   if (length(package)==0) {
     stop("No package specified Error # 5792324572")
-  } else if (package == "GPfit") {
+  } else if (package %in% c("GPfit", "gpfit")) {
     u <- UGP2_GPfit$new(X=X, Z=Z, ...)
-  } else if (package=="laGP") {
+  } else if (package %in% c("laGP", "laGP")) {
     u <- UGP2_laGP$new(X=X, Z=Z, ...)
   } else if (package %in% c("blm","btlm","bcart","bgp","bgpllm","btgp","btgpllm")) {
     u <- UGP2_tgp$new(X=X, Z=Z, ...)
-  } else if (package=="mlegp") {
+  } else if (package %in% c("mlegp")) {
     u <- UGP2_mlegp$new(X=X, Z=Z, ...)
-  } else if (package=="GauPro") {
+  } else if (package %in% c("GauPro", "gaupro")) {
     u <- UGP2_GauPro$new(X=X, Z=Z, ...)
-  } else if (package=="DiceKriging") {
+  } else if (package %in% c("DiceKriging", "dicekriging", "dice", "Dice", "DK", "dk")) {
     u <- UGP2_DiceKriging$new(X=X, Z=Z, ...)
-  } else if (package == "sklearn") {
+  } else if (package  %in% c( "sklearn", "scikit-learn", "scikitlearn")) {
     u <- UGP2_sklearn$new(X=X, Z=Z, ...)
-  } else if (package == "GPy") {
+  } else if (package  %in% c( "GPy", "gpy")) {
     u <- UGP2_GPy$new(X=X, Z=Z, ...)
   } else {
     stop("Package not recognized")
