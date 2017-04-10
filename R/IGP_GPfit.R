a7e3604f958e4bcc3c808919d6cd6180563be437
Collin Erickson <collinberickson@gmail.com>
2017-04-10 17:15:11 -0500

    Added laGP_GauPro, a hybrid that fits with laGP and predicts with GauPro

--- R/IGP_GPfit.R
+++ R/IGP_GPfit.R
@@ -121,7 +121,7 @@ IGP_GPfit <- R6::R6Class(classname = "IGP_GPfit", inherit = IGP_base,
 #'   updates the model, adding new data if given, then running optimization again.}}
 IGP_laGP <- R6::R6Class(classname = "IGP_laGP", inherit = IGP_base,
                             public = list(
-                              .init = function(..., d=NULL, g=NULL, theta=NULL, nugget=NULL, estimate_params=TRUE) {#browser()
+                              .init = function(..., d=NULL, g=NULL, theta=NULL, nugget=NULL, no_update=FALSE) {#browser()
                                 if (self$corr[[1]] != "gauss") {
                                   stop("laGP only uses Gaussian correlation")
                                 }
@@ -144,10 +144,10 @@ IGP_laGP <- R6::R6Class(classname = "IGP_laGP", inherit = IGP_base,
                                 ga_start <- if (!is.null(g)) g else ga$start
                                 mod1 <- laGP::newGPsep(X=self$X, Z=self$Z, d=da_start, g=ga$start, dK = TRUE)
                                 #mod1 <- laGP::newGPsep(X=X, Z=Z, d=da$start, g=1e-6, dK = TRUE)
-                                if (!estimate_params) { #using d and g given
+                                if (no_update) { #using d and g given
                                   self$mod.extra$theta = as.numeric(1 / d) # store theta params
                                   self$mod.extra$nugget = as.numeric(g) # store nugget
-                                } else if (estimate_params && !self$estimate.nugget) { # Only estimate d/theta
+                                } else if (!no_update && !self$estimate.nugget) { # Only estimate d/theta
                                   mle.out <- laGP::mleGPsep(gpsepi = mod1,
                                                             param="d",
                                                              tmin=drange[1], tmax=drange[2],
@@ -155,7 +155,7 @@ IGP_laGP <- R6::R6Class(classname = "IGP_laGP", inherit = IGP_base,
                                   self$mod.extra$theta <- as.numeric(1 / mle.out$d) # store theta params
                                   self$mod.extra$nugget <- g
                                   # Leave nugget as is
-                                } else if (estimate_params) {
+                                } else if (!no_update) { # Update both
                                   mle.out <- laGP::jmleGPsep(gpsepi = mod1,
                                                              drange=drange,
                                                              grange=grange,
@@ -166,8 +166,8 @@ IGP_laGP <- R6::R6Class(classname = "IGP_laGP", inherit = IGP_base,
                                 } else {stop("Shouldn't be here IGP_laGP #32097555")}
                                 self$mod <- mod1
                               }, #"function to initialize model with data
-                              .update = function(..., estimate_params=TRUE) {#browser()
-                                if (!estimate_params) { # just add data and return
+                              .update = function(..., no_update=FALSE) {#browser()
+                                if (no_update) { # just add data and return
                                   laGP::updateGPsep(gpsepi=self$mod,
                                                     X=self$X[-(1:self$n.at.last.update), , drop=FALSE],
                                                     Z=self$Z[-(1:self$n.at.last.update)])
@@ -202,9 +202,9 @@ IGP_laGP <- R6::R6Class(classname = "IGP_laGP", inherit = IGP_base,
                                 }
                                 drange <- c(1e-3,1e4)
                                 grange <- c(min(sqrt(.Machine$double.eps),self$mod.extra$nugget), max(1,self$mod.extra$nugget))
-                                if (!estimate_params) {
+                                if (no_update) {
                                   stop("This is covered above at beginning of .update")
-                                } else if (estimate_params && !self$estimate.nugget) { # update d/theta but not nugget/g
+                                } else if (!no_update && !self$estimate.nugget) { # update d/theta but not nugget/g
                                   mle.try <- try(mle.out <- laGP::mleGPsep(gpsepi = self$mod,
                                                                            param = "d",
                                                                             tmin=drange[1], tmax=drange[2],
@@ -214,13 +214,13 @@ IGP_laGP <- R6::R6Class(classname = "IGP_laGP", inherit = IGP_base,
                                     browser()
                                     warning('Restarting laGP model')
                                     self$delete()
-                                    self$init(..., estimate_params=estimate_params)
+                                    self$init(..., no_update=no_update)
                                     return()
                                   }
                                   # Update stored parameters for when user calls $theta() or $nugget()
                                   self$mod.extra$theta = as.numeric(1 / mle.out$d) # store theta params
                                   # leave nugget as it was
-                                } else if (estimate_params && self$estimate.nugget) {
+                                } else if (!no_update && self$estimate.nugget) {
                                   mle.try <- try(mle.out <- laGP::jmleGPsep(gpsepi = self$mod,
                                                                             #drange=c(da$min, da$max), # Getting rid of these here too
                                                                             #grange=c(ga$min, ga$max),
@@ -1154,4 +1154,110 @@ IGP_ooDACE <- R6::R6Class(classname = "IGP_ooDACE", inherit = IGP_base,
                            } # function that gives mean (constant, other functions not implemented)
 
                          )
+)
+
+
+
+
+#' UGP
+#' Class providing object with methods for fitting a GP model
+#'
+#' @docType class
+#' @importFrom R6 R6Class
+#' @export
+#' @keywords data, kriging, Gaussian process, regression
+#' @return Object of \code{\link{R6Class}} with methods for fitting GP model.
+#' @format \code{\link{R6Class}} object.
+#' @examples
+#' \dontrun{
+#' n <- 40
+#' d <- 2
+#' n2 <- 20
+#' f1 <- function(x) {sin(2*pi*x[1]) + sin(2*pi*x[2])}
+#' X1 <- matrix(runif(n*d),n,d)
+#' Z1 <- apply(X1,1,f1) + rnorm(n, 0, 1e-3)
+#' X2 <- matrix(runif(n2*d),n2,d)
+#' Z2 <- apply(X2,1,f1)
+#' XX1 <- matrix(runif(10),5,2)
+#' ZZ1 <- apply(XX1, 1, f1)
+#' u <- IGP_sklearn$new(X=X1,Z=Z1)
+#' cbind(u$predict(XX1), ZZ1)
+#' u$predict.se(XX1)
+#' u$update(Xnew=X2,Znew=Z2)
+#' u$predict(XX1)
+#' u$delete()
+#' }
+#' @field X Design matrix
+#' @field Z Responses
+#' @field N Number of data points
+#' @field D Dimension of data
+#' @section Methods:
+#' \describe{
+#'   \item{Documentation}{For full documentation of each method go to https://github.com/CollinErickson/UGP/}
+#'   \item{\code{new(X=NULL, Z=NULL, package=NULL,
+#'   estimate.nugget=T, set.nugget=F, ...)}}{This method
+#'   is used to create object of this class with \code{X} and \code{Z} as the data.
+#'   The package tells it which package to fit the GP model.}
+#'   \item{\code{Xall=NULL, Zall=NULL, Xnew=NULL, Znew=NULL, ...}}{This method
+#'   updates the model, adding new data if given, then running optimization again.}}
+IGP_laGP_GauPro <- R6::R6Class(classname = "IGP_lagP_GauPro", inherit = IGP_base,
+                           public = list(
+                             .init = function(...) {#browser()
+                               # Fit model to data with laGP
+                               self$mod.extra$laGP <- IGP(X=self$X, Z=self$Z, package="laGP",
+                                                          corr=self$corr,
+                                                          estimate.nugget=self$estimate.nugget,
+                                                          set.nugget=self$set.nugget)
+                               #self$mod.extra$laGP$init(X=self$X, Z=self$Z, ...)
+
+                               # Copy params to GauPro, don't fit, use this for predicting
+                               self$mod.extra$GauPro <- IGP(X=self$X, Z=self$Z, package="GauPro",
+                                                          corr=self$corr,
+                                                          estimate.nugget=FALSE,
+                                                          set.nugget=self$mod.extra$laGP$nugget(),
+                                                          theta=self$mod.extra$laGP$theta(),
+                                                          #nug=self$mod.extra$laGP$nug,
+                                                          param.est=FALSE)
+                               #self$mod.extra$GauPro$init(X=self$X, Z=self$Z,
+                               #                          theta=self$mod.extra$laGP$theta,
+                               #                           nug=self$mod.extra$laGP$nug)
+                               self$mod <- "laGP model is mod.extra$laGP, GauPro model is mod.extra$GauPro. This fits with laGP but predicts with GauPro"
+                             }, #"function to initialize model with data
+                             .update = function(..., no_update=FALSE) {
+                               # Update model in laGP
+                               if (!no_update) { # won't have it update data even not updating params since I don't like when it gives issues, and we pass Xall anyways
+                                 self$mod.extra$laGP$update(Xall=self$X, Zall=self$Z,
+                                                            no_update=no_update,
+                                                            ...)
+                               }
+                               # Pass GauPro new theta and nugget if it was updated
+                               if (!no_update) {
+                                 self$mod.extra$GauPro$mod$theta <- self$mod.extra$laGP$theta()
+                                 self$mod.extra$GauPro$mod$nug <- self$mod.extra$laGP$nugget()
+                               }
+                               self$mod.extra$GauPro$update(Xall=self$X, Zall=self$Z,
+                                                            no_update=TRUE)
+
+                             }, #"function to add data to model or reestimate params
+                             .predict = function(XX, se.fit, ...) {
+                               self$mod.extra$GauPro$.predict(XX=XX, se.fit=se.fit, ...)
+                             }, #"function to predict at new values
+                             .predict.se = function(XX, ...) {
+                               self$mod.extra$GauPro$.predict.se(XX=XX, ...)
+                             }, #"function predict the standard error/dev
+                             .predict.var = function(XX, ...) {
+                               self$mod.extra$GauPro$.predict.var(XX=XX, ...)
+                             }, #"function to predict the variance
+                             .grad = function(XX) {self$mod.extra$GauPro$grad(XX=XX)}, # function to calculate the gradient
+                             .delete = function(...){
+                               self$mod.extra$laGP$delete()
+                               self$mod.extra$GauPro$delete()
+                               self$mod.extra <- NULL
+                               self$mod <- NULL
+                             }, #"function to delete model beyond simple deletion
+                             .theta = function() {self$mod.extra$GauPro$theta()}, #"function to get theta, exp(-theta*(x-x)^2)
+                             .nugget = function() {self$mod.extra$GauPro$nugget()}, #"function to get nugget
+                             .mean = NULL # function that gives mean (constant, other functions not implemented)
+
+                           )
 )
