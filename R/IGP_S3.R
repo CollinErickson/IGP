42ed886533dfd29ac95b7f3b92ed1f90b751ff80
Collin Erickson <collinberickson@gmail.com>
2017-09-22 20:56:23 -0500

    Fixed error

--- R/IGP_S3.R
+++ R/IGP_S3.R
@@ -1,6 +1,8 @@
 #' Predict for class IGP
 #'
 #' @param object Object of class IGP
+#' @param XX Points to predict at
+#' @param se.fit Whether the standard error prediction should be returned with the mean prediction
 #' @param ... Additional parameters
 #'
 #' @return Prediction from object at XX
@@ -12,6 +14,6 @@
 #' y <- sin(2*pi*x) + rnorm(n,0,1e-1)
 #' gp <- IGP(package='laGP', X=x, Z=y, parallel=FALSE)
 #' predict(gp, .448)
-predict.IGP <- function(object, XX, se.fit=F, covmat=F, split_speed=T, ...) {
+predict.IGP <- function(object, XX, se.fit=FALSE, ...) {
   object$predict(XX=XX, se.fit=se.fit)
 }
